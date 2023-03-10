dfde18780ab9da02f98c8f47cd88c318ed67fb99
Godknows Christians Nyamatendedza <godycnyama@gmail.com>
2023-03-10 09:39:45 +0200

    added EntityFrameworkCoee SQLite

---
+++ RugbyManager.Persistence/RugbyManagerContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using RugbyManager.Domain.Models;
+
+namespace RugbyManager.Persistence
+{
+    public class RugbyManagerContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Stadium> Stadiums { get; set; }
+        public string DbPath { get; } = String.Empty;
+
+        public RugbyManagerContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
